8ab149d9f626b69f8748f91a7215c6dff58bf78e
acgotaku <acgotaku311@gmail.com>
2023-04-16 10:18:26 +0900

    feat: flip animation

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useCallback } from 'react';
+import React, { useRef } from 'react';
 import cls from 'clsx';
 import { noop } from '@/utils/misc';
 import { useDraggable } from '@/hooks/useDraggable';
@@ -16,17 +16,6 @@ const List: React.FC<IListProps> = ({
   itemClassName = ''
 }) => {
   const containerRef = useRef<HTMLUListElement>(null);
-  const prevRects = useRef<Record<string, DOMRect>>({});
-  const recordRect = useCallback(() => {
-    if (containerRef.current) {
-      Array.from(containerRef.current.children).forEach(async node => {
-        const dom = node as HTMLElement;
-        const key = dom.dataset.id as string;
-        const rect = dom.getBoundingClientRect();
-        prevRects.current[key] = rect;
-      });
-    }
-  }, []);
 
   const {
     sortedData,
@@ -38,41 +27,9 @@ const List: React.FC<IListProps> = ({
   } = useDraggable({
     dataSource,
     updateData: setList,
-    onDragStart: recordRect
+    draggable,
+    containerRef
   });
-
-  useEffect(() => {
-    if (containerRef.current) {
-      Array.from(containerRef.current.children).forEach(async node => {
-        const dom = node as HTMLElement;
-        const key = dom.dataset.id as string;
-        const prevRect = prevRects.current[key];
-        const rect = dom.getBoundingClientRect();
-        if (prevRect) {
-          const dy = prevRect.y - rect.y;
-          const dx = prevRect.x - rect.x;
-          dom.style.pointerEvents = 'none';
-          dom.animate(
-            [
-              {
-                transform: `translate(${dx}px, ${dy}px)`
-              },
-              { transform: 'translate(0, 0)' }
-            ],
-            {
-              duration: 300,
-              easing: 'linear'
-            }
-          );
-          await Promise.allSettled(
-            node.getAnimations().map(animation => animation.finished)
-          );
-          dom.style.pointerEvents = '';
-        }
-        prevRects.current[key] = rect;
-      });
-    }
-  }, [sortedData]);
 
   return (
     <div className={styles.list}>
